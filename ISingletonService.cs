54efec7bac78a2750cd9c88618c3bc43192d0a5e
Jeff Vera <jeff.vera@gmail.com>
2011-07-07 18:49:25 -0400

    - Initial commit.

---
+++ ISingletonService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Singleton
+{
+	[ServiceContract]
+	public interface ISingletonService
+	{
+		[OperationContract]
+		string GetData(int value);
+	}
+
+}
---
+++ SingletonService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Singleton
+{
+	public class SingletonService : ISingletonService
+	{
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+	}
+}
